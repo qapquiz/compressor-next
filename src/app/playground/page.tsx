4fcb3f379cacbfa08c4bd3ba9cec14824c15b28c
qapquiz <arm.armariya@gmail.com>
2024-10-09 02:15:19 +0700

    fix: delete PlaoygroundPage

--- src/app/playground/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CompressedSwapCard } from "@/components/CompressedSwapCard";
-
-export default function PlaygroundPage() {
-	return (
-		<div className="h-full w-full">
-			<CompressedSwapCard />
-		</div>
-	);
-}
